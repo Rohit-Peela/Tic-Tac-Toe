37d135674286d62329e516b8b42ae0bbd7a8478d
peela <rohitpeela1998@gmail.com>
2021-11-26 16:57:27 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+const date = new Date().getFullYear();
+
+    const fontChange = {
+        fontFamily: "Nunito",
+        textAlign:"center"
+    }
+
+class Footer extends Component {
+  
+  render() {
+    return (
+      <footer className="footer">
+         <label style={fontChange}>Copyright &copy; {date}, Rohit Peela</label>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
